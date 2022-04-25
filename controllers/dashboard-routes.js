2f2c99f869bcfb3b13eb7e0377829cb96d5265c2
JESSE <dtrsjesse121@gmail.com>
2022-04-25 14:47:02 -0400

    setup filebase and intitial coomit. Add login, logout, main, and dashboard routes.

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, (req, res) => {
+  console.log(req.session);
+  console.log('======================');
+  res.render('dashboard', {loggedIn: true})
+});
+
+module.exports = router;
