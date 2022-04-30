5bc4426d8e5308037c754a0f49848035dbf5d974
JESSE <dtrsjesse121@gmail.com>
2022-04-30 11:11:49 -0400

    syntax fix

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,25 @@ const sequelize = require('../config/connection');
 const { User } =  require('../models')
 
 router.get('/', (req, res) => {
-    res.render('homepage', {loggedIn: req.session.loggedIn})
+  Joke.findAll({
+    attributes: [
+      'id',
+      'joke_body',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE joke.id = vote.joke_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(dbPostData => {
+    const jokes = dbPostData.map(joke => joke.get({ plain: true }));
+    res.render('homepage', { jokes, loggedIn: true });
+  })
 })
 
 router.get('/login', (req, res) => {
